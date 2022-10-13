ce0a98b99b583b9e10273a7b56787eedaa11e0c8
doksyilji <chatdoks@gmail.com>
2022-10-13 05:29:23 -0700

    changed the placeholders in 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("\"Size of a char: %lu bytes(s)", sizeof(char));
-	printf("\"Size of an int: %lu bytes(s)", sizeof(int));
-	printf("\"Size of a long int: %lu bytes(s)", sizeof(long int));
-	printf("\"Size of a long long int: %lu bytes(s)", sizeof(long long int));
-	printf("\"Size of a float: %lu bytes(s)", sizeof(float));
+	printf("Size of a char: %zu bytes(s)", sizeof(char));
+	printf("Size of an int: %zu bytes(s)", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)", sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)", sizeof(float));
 	return (0);
 }
