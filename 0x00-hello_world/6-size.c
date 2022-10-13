36e1cefc6bd2c329fb97ae4561c6134992735cc0
doksyilji <chatdoks@gmail.com>
2022-10-13 06:59:42 -0700

    v19

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	fprintf(stderr, "Size of a char: %zu bytes(s)\n", sizeof(char));
-	fprintf(stderr, "Size of an int: %zu bytes(s)\n", sizeof(int));
-	fprintf(stderr, "Size of a long int: %zu bytes(s)\n", sizeof(long int));
-	fprintf(stderr, "Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
-	fprintf(stderr, "Size of a float: %zu bytes(s)\n", sizeof(float));
-	return (0);
+printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %zu bytes(s)\n", sizeof(float));
+return (0);
 }
