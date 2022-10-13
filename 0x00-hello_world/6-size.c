f555df3c90cefe968e95a8e23bc92ab8697d5aa2
doksyilji <chatdoks@gmail.com>
2022-10-13 07:34:42 -0700

    v24

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,16 @@ size_t anint = sizeof(int);
 size_t alongint = sizeof(long int);
 size_t along2int = sizeof(long long int);
 size_t afloat = sizeof(float);
-char s1[] = sprintf("Size of a char: %zu bytes(s)", achar);
-char s2[] = sprintf("Size of an int: %zu bytes(s)", anint);
-char s3[] = sprintf("Size of a long int: %zu bytes(s)", alongint);
-char s4[] = sprintf("Size of a long long int: %zu bytes(s)", along2int);
-char s5[] = sprintf("Size of a float: %zu bytes(s)", afloat);
+char s1[80];
+char s2[80];
+char s3[80];
+char s4[80];
+char s5[80]
+sprintf(s1,"Size of a char: %zu bytes(s)\0", achar);
+sprintf(s2,"Size of an int: %zu bytes(s)\0", anint);
+sprintf(s3,"Size of a long int: %zu bytes(s)\0", alongint);
+sprintf(s4,"Size of a long long int: %zu bytes(s)\0", along2int);
+sprintf(s5,"Size of a float: %zu bytes(s)\0", afloat);
 puts(s1)
 puts(s2)
 puts(s3)
