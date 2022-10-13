0f99b73499cc80de3b6dcb3a7774d028c4110f45
doksyilji <chatdoks@gmail.com>
2022-10-13 07:21:04 -0700

    made few changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,16 @@
  */
 int main(void)
 {
-char s1[] = sprintf("Size of a char: %zu bytes(s)", sizeof(char));
-char s2[] = sprintf("Size of an int: %zu bytes(s)", sizeof(int));
-char s3[] = sprintf("Size of a long int: %zu bytes(s)", sizeof(long int));
-char s4[] = sprintf("Size of a long long int: %zu bytes(s)", sizeof(long long int));
-char s5[] = sprintf("Size of a float: %zu bytes(s)", sizeof(float));
+achar = sizeof(char);
+anint = sizeof(int);
+alongint = sizeof(long int);
+along2int = sizeof(long long int);
+afloat = sizeof(float;)
+char s1[] = sprintf("Size of a char: %zu bytes(s)", achar);
+char s2[] = sprintf("Size of an int: %zu bytes(s)", anint);
+char s3[] = sprintf("Size of a long int: %zu bytes(s)", alongint);
+char s4[] = sprintf("Size of a long long int: %zu bytes(s)", along2int);
+char s5[] = sprintf("Size of a float: %zu bytes(s)", afloat);
 puts(s1)
 puts(s2)
 puts(s3)
