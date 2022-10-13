596d9d7c75c2033fe2e1a99cf8ba51c07a235ed1
doksyilji <chatdoks@gmail.com>
2022-10-13 08:40:58 -0700

    v32

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -8,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "\"and that piece of art is useful
-			\"- Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
