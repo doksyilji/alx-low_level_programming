17b11945c14c2eada2e9068020832934699daa53
doksyilji <chatdoks@gmail.com>
2022-10-13 07:17:09 -0700

    v22

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,15 @@
  */
 int main(void)
 {
-printf("\"Size of a char: %zu bytes(s)", sizeof(char));
-printf("\"Size of an int: %zu bytes(s)", sizeof(int));
-printf("\"Size of a long int: %zu bytes(s)", sizeof(long int));
-printf("\"Size of a long long int: %zu bytes(s)", sizeof(long long int));
-printf("\"Size of a float: %zu bytes(s)", sizeof(float));
+char s1[] = sprintf("Size of a char: %zu bytes(s)", sizeof(char));
+char s2[] = sprintf("Size of an int: %zu bytes(s)", sizeof(int));
+char s3[] = sprintf("Size of a long int: %zu bytes(s)", sizeof(long int));
+char s4[] = sprintf("Size of a long long int: %zu bytes(s)", sizeof(long long int));
+char s5[] = sprintf("Size of a float: %zu bytes(s)", sizeof(float));
+puts(s1)
+puts(s2)
+puts(s3)
+puts(s4)
+puts(s5)
 return (0);
 }
