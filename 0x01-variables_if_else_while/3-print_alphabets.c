25751cede5090d3093294682a2599643e9af65a1
doksyilji <chatdoks@gmail.com>
2022-10-16 04:12:13 -0700

    0 - 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	for (int x = 'A'; x <= 'z'; x++)
+	{
+		putchar(tolower(o));
+		putchar('\n');
+	}
+	return (0);
+}
