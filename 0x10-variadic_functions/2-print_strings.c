bcb334a67cf783bd1c3cc764b5b822a4a437eb5e
doksyilji <chatdoks@gmail.com>
2022-11-18 06:54:00 -0800

    adding variatic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *print_strings - Prints numbers, followed by a new line
+ *@separator: String to be printed between strings
+ *@n: Amount of numbers given to the function to print
+ *Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strs;
+	unsigned int i;
+	const char *str;
+
+	va_start(strs, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strs, const char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < (n - 1) && separator != 0)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(strs);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Learn Variatic functions in C programming.
